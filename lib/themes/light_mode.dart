2354759e42cda8df85a2213c582b5645b6a96d3d
Mohamed Zaghloul <zaghloulmohamed9@gmail.com>
2025-01-22 13:26:34 +0200

    Updated profile page UI and adjusted necessary models

--- lib/themes/light_mode.dart
+++ lib/themes/light_mode.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 ThemeData lightMode = ThemeData(
   colorScheme: ColorScheme.light(
     surface: Colors.grey.shade300,
-    primary: Colors.grey.shade500,
-    secondary: Colors.grey.shade100,
+    primary: Colors.black,
+    secondary: Colors.red.shade100,
     tertiary: Colors.white,
     inversePrimary: Colors.grey.shade700,
   )
