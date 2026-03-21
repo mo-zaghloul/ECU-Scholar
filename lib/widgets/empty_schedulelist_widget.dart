aa29475c963851b7cb8b10adb5a48c97d1eb4ece
Mohamed Zaghloul <zaghloulmohamed9@gmail.com>
2026-03-21 10:36:44 +0200

    Refactor EmptySchedulelistWidget to use SVG assets and improve layout
    - Replaced the static image with an SVG asset that changes based on the theme (light/dark mode).
    - Adjusted the layout to center the SVG and text, enhancing visual appeal.
    - Updated text styling and spacing for better readability.

--- lib/widgets/empty_schedulelist_widget.dart
+++ lib/widgets/empty_schedulelist_widget.dart
@@ -1,20 +1,41 @@
 import 'package:ecu_scholar/constants/text_styles.dart';
+import 'package:ecu_scholar/themes/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:provider/provider.dart';
 
 class EmptySchedulelistWidget extends StatelessWidget {
   const EmptySchedulelistWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final isDark = Provider.of<ThemeProvider>(context).isDarkMode;
+    final svgAsset =
+        isDark ? 'assets/svgs/hot-air-balloon-light.svg' : 'assets/svgs/hot-air-balloon-dark.svg';
+
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       children: <Widget>[
-        Image.asset('assets/images/Navigate_Rock_the_boat.png'),
-        const SizedBox(height: 16),
-        Text(
-          'It looks like you have a free day. \n Enjoy the break!',
-          textAlign: TextAlign.center,
-          style: AppTextStyles.bodyText1,
+        const SizedBox(height: 40),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(svgAsset, width: 200, height: 200),
+          ],
+        ),
+        const SizedBox(height: 24),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: Text(
+                'It looks like you have a free day.\nEnjoy the break!',
+                textAlign: TextAlign.center,
+                style: AppTextStyles.bodyText1,
+              ),
+            ),
+          ],
         ),
       ],
     );
